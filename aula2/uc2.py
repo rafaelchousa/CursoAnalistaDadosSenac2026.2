23f0a0988cb2142464bba175cc54562dbe6382cd
RAFAEL CHOUSA DE FIGUEIREDO <rafael.chousa@aluno.senac.rio>
2026-08-21 21:20:03 -0300

    versionamento

--- aula2/uc2.py
+++ aula2/uc2.py
@@ -7,7 +7,30 @@
 
 # print(idade)
 
-# algoritmo  BOLETIM:
+#Desafio1:
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+# Determinar ordem
+if num1 <= num2 and num1 <= num3:
+    if num2 <= num3:
+        print(f"{num1}, {num2}, {num3}")
+    else:
+        print(f"{num1}, {num3}, {num2}")
+elif num2 <= num1 and num2 <= num3:
+    if num1 <= num3:
+        print(f"{num2}, {num1}, {num3}")
+    else:
+        print(f"{num2}, {num3}, {num1}")
+else:
+    if num1 <= num2:
+        print(f"{num3}, {num1}, {num2}")
+    else:
+        print(f"{num3}, {num2}, {num1}")
+
+
+# algoritmo  BOLETIM Desafio2:
 nota1 = 2
 nota2 = 4
 
--- aula5/uc5.py
+++ aula5/uc5.py
@@ -1,2 +1,37 @@
-for 1 in range (10):
-    print(1)
+# for i in range (-102,-1,2):
+#     print(i+0,500)
+
+#WHILE
+# somador= int(input("Registro:"))
+# controle= 0
+
+# while controle <= 30:
+#     controle=controle+somador
+#     somador = int(input("Registro:"))
+
+# print("Oficina lotada!")
+# print("Quantidade:"  ,controle)
+# O range(5) gera os números 0, 1, 2, 3, 4 (5 repetições)
+# for i in range(5):
+      
+ # i representa o número atual da repetição (0, 1, 2...)
+
+acertou = 0
+while acertou <= 5:
+    print(f"Número {acertou + 1} de 5:")
+    num = float(input("Digite um número: "))
+ 
+    dobro = num * 2
+    triplo = num * 3
+    quádruplo = num * 4
+ 
+    print(f" Resultado: Dobro={dobro}, Triplo={triplo}, Quádruplo={quádruplo}\n")
+ 
+print("Entrada inválida. Tente novamente.")
+num=float(input("Digite um número:"))
+acertou = acertou+1
+
+
+
+
+    
